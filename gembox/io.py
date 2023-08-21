91b2ef7ff3b89c92dd3109dbf12ec09ab8249f82
stevieflyer <steveflyer7@gmail.com>
2023-08-21 23:28:16 +0800

    add list_dir

--- gembox/io.py
+++ gembox/io.py
@@ -1,4 +1,5 @@
 import pathlib
+from typing import List, Union
 
 
 def ensure_pathlib_path(path):
@@ -23,6 +24,24 @@ def check_and_make_dir(dir_path):
     :return: (pathlib.Path) the path to the directory
     """
     dir_path = ensure_pathlib_path(dir_path)
+    # check whether the path is a directory
     if not dir_path.exists():
         dir_path.mkdir(parents=True, exist_ok=True)
+    else:
+        # already exists, check whether it is a directory
+        if not dir_path.is_dir():
+            raise ValueError(f"Path {dir_path} is not a directory")
     return dir_path
+
+
+def list_dir(dir_path: Union[str, pathlib.Path]) -> List[pathlib.Path]:
+    """
+    List out all filepath under a directory
+
+    :param dir_path: (str | pathlib.Path) the path to the directory
+    :return: (List[pathlib.Path]) the list of filepaths
+    """
+    dir_path = ensure_pathlib_path(dir_path)
+    if not dir_path.is_dir():
+        raise ValueError(f"Path {dir_path} is not a directory")
+    return list(dir_path.iterdir())
