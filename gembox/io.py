7178233a2aa19ce8555d5bb12d541c866fc9cfce
stevieflyer <steveflyer7@gmail.com>
2023-08-22 00:31:22 +0800

    add list_dir extension

--- gembox/io.py
+++ gembox/io.py
@@ -1,3 +1,4 @@
+import re
 import pathlib
 from typing import List, Union
 
@@ -34,14 +35,26 @@ def check_and_make_dir(dir_path):
     return dir_path
 
 
-def list_dir(dir_path: Union[str, pathlib.Path]) -> List[pathlib.Path]:
+def list_dir(dir_path: Union[str, pathlib.Path], file_only: bool = False, dir_only: bool = False, regexp: str = None) -> List[pathlib.Path]:
     """
     List out all filepath under a directory
 
     :param dir_path: (str | pathlib.Path) the path to the directory
+    :param file_only: (bool) whether to list out only files
+    :param dir_only: (bool) whether to list out only directories
+    :param regexp: (str) the regular expression to filter the file/directory name
     :return: (List[pathlib.Path]) the list of filepaths
     """
+    if file_only and dir_only:
+        raise ValueError("file_only and dir_only cannot be both True")
     dir_path = ensure_pathlib_path(dir_path)
     if not dir_path.is_dir():
         raise ValueError(f"Path {dir_path} is not a directory")
-    return list(dir_path.iterdir())
+    if regexp is not None:
+        regexp = re.compile(regexp)
+    if file_only:
+        return [x for x in dir_path.iterdir() if x.is_file() and (regexp is None or regexp.search(x.name))]
+    elif dir_only:
+        return [x for x in dir_path.iterdir() if x.is_dir() and (regexp is None or regexp.search(x.name))]
+    else:
+        return [x for x in dir_path.iterdir() if (regexp is None or regexp.search(x.name))]
